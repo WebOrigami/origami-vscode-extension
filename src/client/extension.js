ff6764b9aa45afb1abe351fe14d40adaf96b011a
Jan Miksovsky <jan@miksovsky.com>
2025-03-13 17:14:57 -0700

    Scaffolding for LSP

---
+++ src/client/extension.js
@@ -0,0 +1,69 @@
+import * as path from "path";
+import { ExtensionContext, workspace } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+/** @type {LanguageClient} */
+let client;
+
+/**
+ *
+ * @param {ExtensionContext} context
+ */
+export function activate(context) {
+  // The server is implemented in node
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  /** @type {ServerOptions} */
+  const serverOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+  };
+
+  // Options to control the language client
+  /** @type {LanguageClientOptions} */
+  const clientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [
+      { scheme: "file", language: "origami" },
+      { scheme: "untitled", language: "origami" },
+    ],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate() {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
