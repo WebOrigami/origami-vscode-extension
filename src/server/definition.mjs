9951c4d91e0b271813f2fc9f8206b8bdeb89d486
Jan Miksovsky <jan@miksovsky.com>
2025-03-19 10:55:11 -0700

    Go to Definition supports root directory and home directory

--- src/server/definition.mjs
+++ src/server/definition.mjs
@@ -1,5 +1,6 @@
 import { FileTree } from "@weborigami/async-tree";
 import path from "node:path";
+import process from "node:process";
 import { fileURLToPath, pathToFileURL } from "node:url";
 
 /**
@@ -78,6 +79,21 @@ export default async function definition(
 // Find the key in the project scope, starting at the given folder path and
 // walking up to one of the workspace roots or the file system root.
 async function findInProjectScope(key, folderPath, workspaceFolderPaths) {
+  // Special cases
+  if (key === "") {
+    // root folder
+    return {
+      path: "/",
+      value: new FileTree("/"),
+    };
+  } else if (key === "~") {
+    // home folder
+    return {
+      path: process.env.HOME,
+      value: new FileTree(process.env.HOME),
+    };
+  }
+
   let currentPath = folderPath;
   while (currentPath !== "/") {
     const fileTree = new FileTree(currentPath);
